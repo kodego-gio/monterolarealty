9b2fd67d9c005749b0729746b362abafbbcd27eb
Giovanni Monterola <giovanni.filipinohomes@gmail.com>
2023-10-27 13:56:18 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Header } from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My First React</h1>
-    </div>
+    <div>
+      <Header />
+   </div>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button, Container, Form, Nav, NavDropdown, Navbar} from 'react-bootstrap'
+
+export const Header = () => {
+  return (
+    
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Monterola Realty</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown title="Find More" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">For Sale</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                For Rent
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">For Closure</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Project Selling
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/loginSignup">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="/register">
+              Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+ 
+  )
+}
