270b657e4b5cc5aafe5885670095992fdba22e90
Giovanni Monterola <giovanni.filipinohomes@gmail.com>
2023-11-20 19:29:52 +0800

    changed API URL in Login.js file

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,23 +1,83 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [submitLoading, setsubmitLoading] = useState(false);
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  const [error, setError] = useState('');
+
+   const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError({ ...error, [e.target.name]: '' });
+  };
+  
+    const handleInput = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your login logic here
-    console.log(formData);
+   const handleSubmit = async (event) => {
+     event.preventDefault();
+    setsubmitLoading(true);
+    setError('');
+     if (!validateForm()) {
+      console.log("checking...if");
+      setsubmitLoading(false);
+      console.log("checking...if statement");
+      return;
+    } else
+     {
+       console.log("checking...else");
+      try {
+        //call apiBackend
+        //http://127.0.0.1:8000/api/login
+        console.log("checking...try...");
+        const response = await axios.post('http://localhost:8000/api/login', formData, {
+          headers: {
+            'Accept': 'application/json'
+          }
+        });
+        
+        console.log("respo",response);
+        const token = response.data.token;
+        setsubmitLoading(false);
+        localStorage.setItem('login_token', token);
+        console.log(response);
+        navigate('/addproperty');
+        
+      } catch (error) {
+        console.log("checking...catch");
+        console.log("error=>",error);
+        setsubmitLoading(false);
+        //setError(error.response.data.message);
+        
+      }
+    }
+  };
+
+  const validateForm = () => {
+    console.log("checking...validateFOrm");
+    if (formData.email === undefined || formData.email === '') {
+      console.log('Email is required');
+      setError('Email is mandatory!');
+      return false;
+    }
+    if (formData.password === undefined || formData.password === '') {
+      console.log('Password is required');
+      setError('Password is mandatory!');
+      return false;
+    }
+
+    return true;
   };
 
   return (
@@ -31,7 +91,7 @@ const LoginForm = () => {
             id="email"
             name="email"
             value={formData.email}
-            onChange={handleChange}
+            onChange={handleInput}
             required
           />
         </div>
@@ -42,7 +102,7 @@ const LoginForm = () => {
             id="password"
             name="password"
             value={formData.password}
-            onChange={handleChange}
+            onChange={handleInput}
             required
           />
         </div>
